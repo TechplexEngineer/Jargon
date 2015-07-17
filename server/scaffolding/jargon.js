9542d99fb7d0dc6b3a60dfe6f76a57b1b07791a1
Blake Bourque <Techplex.Engineer@gmail.com>
2015-07-17 12:17:47 -0400

    Properly show the link

--- server/scaffolding/jargon.js
+++ server/scaffolding/jargon.js
@@ -4,21 +4,21 @@ if (Jargon.find().count() == 0) {
 		name: 'FTP', // name must be unique
 		lname: "File Transfer Protocol",
 		descr: "A method used to transmit files between computers",
-		url: 'ftp.example.com',
+		url: 'http://ftp.example.com',
 		related: ['SFTP', 'SCP']
 	});
 	Jargon.insert({
 		name: 'SFTP', // name must be unique
 		lname: "Secure File Transfer Protocol",
 		descr: "A method used to transmit files between computers somewhat securely",
-		url: 'sftp.example.com',
+		url: 'http://sftp.example.com',
 		related: ['FTP', 'SCP']
 	});
 	Jargon.insert({
 		name: 'SCP', // name must be unique
 		lname: "Secure Copy Protocol",
 		descr: "A method used to transmit files between computers somewhat securely",
-		url: 'sftp.example.com',
+		url: 'http://scp.example.com',
 		related: ['SFTP','FTP']
 	});
 }
