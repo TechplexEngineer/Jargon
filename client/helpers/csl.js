b97f322d0d3bbcd0538073f577e1fa8ba7fce1e7
Blake Bourque <Techplex.Engineer@gmail.com>
2015-07-18 19:37:26 -0400

    Setup linking and individual pages

--- client/helpers/csl.js
+++ client/helpers/csl.js
@@ -1,9 +1,14 @@
 UI.registerHelper('csl', function(context, options) {
-	console.log(context, _.isArray(context));
+	console.log("Options", options);
 	if(context && _.isArray(context)) {
 		var out = "";
 		for (var i = 0; i < context.length; i++) {
-			out += context[i];
+			if (options && _.isString(options)) {
+				out += "<a href=\"/"+options+"/"+context[i]+"\">"+context[i]+"</a>";
+			} else {
+				out += context[i];
+			}
+			
 			if (i != context.length-1)
 				out += ", ";
 		}
@@ -12,3 +17,4 @@ UI.registerHelper('csl', function(context, options) {
 		return context;
 	}
 });
+
